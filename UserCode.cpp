19ff88ed7d1b0c77239eab416b01bdabb086b7f1
daniel municio <danielmunicio360@gmail.com>
2024-11-08 05:29:13 -0800

    Added Mixer Matrix

--- UserCode.cpp
+++ UserCode.cpp
@@ -35,9 +35,9 @@ const float gravity = 9.81f;  // acceleration of gravity [m/s^2]
 const float inertia_xx = 16e-6f;  //MMOI about x axis [kg.m^2]
 const float inertia_yy = inertia_xx;  //MMOI about y axis [kg.m^2]
 const float inertia_zz = 29e-6f;  //MMOI about z axis [kg.m^2]
-
+const float l = 33e-3f; // Propeller Distance
 const float dt = 1.0f / 500.0f; //[s] period between successive calls to MainLoop
-
+const float k = 0.01;
 // Main Loop
 MainLoopOutput MainLoop(MainLoopInput const &in) {
   //Define the output numbers (in the struct outVals):
@@ -49,6 +49,10 @@ MainLoopOutput MainLoop(MainLoopInput const &in) {
   outVals.motorCommand3 = 0;
   outVals.motorCommand4 = 0;
 
+
+
+  // Sensor Bias Calculation
+  // -----------------------------------------------------------------------------------
   // Use the first second to calibrate the Gyroscope
   // Get the approx error by averaging Gyroscope readings from the first second (dt = 1/500)
   if (in.currentTime < 1.0f) {
@@ -56,11 +60,16 @@ MainLoopOutput MainLoop(MainLoopInput const &in) {
   }
   // Correct Gyroscope value by subtracting Bias
   Vec3f rateGyro_corr = in.imuMeasurement.rateGyro - estGyroBias;
+  // ---------------------------------------------------------------------------------------
+
 
   // Store our IMU Measurements
   lastMainLoopInputs.imuMeasurement.accelerometer = in.imuMeasurement.accelerometer;
   lastMainLoopInputs.imuMeasurement.rateGyro = in.imuMeasurement.rateGyro;
 
+
+  // Attitude Estimation:
+  // ------------------------------------------------------------------------
   // Estimate Roll Pitch and Yaw using corrected integrator and accelerometer measurements
   float phiMeasured = in.imuMeasurement.accelerometer.y / gravity;
   float thetaMeasured = -1.0f * in.imuMeasurement.accelerometer.x / gravity;
@@ -68,17 +77,51 @@ MainLoopOutput MainLoop(MainLoopInput const &in) {
   estPitch = (1.0f - rho) * (estPitch + dt * rateGyro_corr.y) +  (rho * thetaMeasured);
   estRoll = (1.0f - rho) * (estRoll + dt * rateGyro_corr.x) + (rho * phiMeasured);
   estYaw = estYaw + dt * rateGyro_corr.z;
+  // -------------------------------------------------------------------------
 
+  // Inputs to Mixer Matrix
+  Vec3f desTorque = Vec3f(0, 0, 0);
+  float totalForce = 8.0f;
+
+
+  // Mixer Matrix
+  // ------------------------------------------------------------------------
+  float cp1 = (0.25) * (totalForce + (desTorque.x / l) + (desTorque.y / -1.0f * l) + (desTorque.z / k));
+  float cp2 = (0.25) * (totalForce + (desTorque.x / -1.0f * l) + (desTorque.y / -1.0f * l) + (-1.0f * desTorque.z / k));
+  float cp3 = (0.25) * (totalForce + (desTorque.x / -1.0f * l) + (desTorque.y / l) + (desTorque.z / k));
+  float cp4 = (0.25) * (totalForce + (desTorque.x / l) + (desTorque.y / l) + (desTorque.z / -1.0f * k));
+
+  // -------------------------------------------------------------------------
+
+  // Convert Forces -> Propeller Speedes -> PWM Commands
+  // --------------------------------------------------
+  float speed1 = speedFromForce(cp1);
+  float speed2 = speedFromForce(cp2);
+  float speed3 = speedFromForce(cp3);
+  float speed4 = speedFromForce(cp4);
+
+  int pwm1 = pwmCommandFromSpeed(speed1);
+  int pwm2 = pwmCommandFromSpeed(speed2);
+  int pwm3 = pwmCommandFromSpeed(speed3);
+  int pwm4 = pwmCommandFromSpeed(speed4);
+  // -------------------------------------------------
+
+  // Send Values out if blue button is pressed
+  if (in.joystickInput.buttonBlue == true) {
+    outVals.motorCommand1 = pwm1;
+    outVals.motorCommand2 = pwm2;
+    outVals.motorCommand3 = pwm3;
+    outVals.motorCommand4 = pwm4;
+  }
 
   // Log values in the Log output folder
   outVals.telemetryOutputs_plusMinus100[0] = estRoll;
   outVals.telemetryOutputs_plusMinus100[1] = estPitch;
   outVals.telemetryOutputs_plusMinus100[2] = estYaw;
 
-  // If we press the blue button, reset the pitch. This is just so we didn’t have to disconnect the quadcopter between trials.
-  if (in.joystickInput.buttonBlue == true) {
-  estPitch = 0.0f;
-  }
+
+
+
   return outVals;
 }
 
