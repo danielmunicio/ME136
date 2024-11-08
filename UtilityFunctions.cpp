95c18385d882046176de91852a458a65b2845b54
daniel municio <danielmunicio360@gmail.com>
2024-11-08 05:01:49 -0800

    Adding Utility Functions

--- UtilityFunctions.cpp
+++ UtilityFunctions.cpp
@@ -6,24 +6,27 @@ int pwmCommandFromSpeed(float desiredSpeed_rad_per_sec) {
   // number -- this ensures that we use single precision
   // floating point (rather than double precision, which
   // would be substantially slower on the microcontroller).
-  float a = 0.0f;  // the zeroth order term
-  float b = 0.0f;  // the first order term
+  float a = -106.0f;  // the zeroth order term
+  float b = 0.18f;  // the first order term
 
+  // Check if the PWM is above 0, if not, return 0
+  if (int(a + b * desiredSpeed_rad_per_sec) > 0) {
   return int(a + b * desiredSpeed_rad_per_sec);
+  } else {
+  return 0;
+  }
 }
 
 float speedFromForce(float desiredForce_N) {
-  // replace this with your determined constant:
   // Remember to add the trailing "f" for single
   // precision!
-  float const propConstant = 1.0e-08f;
+  float const propConstant = 5.46e-08;
 
   //we implement a safety check,
   //  (no sqrtf for negative numbers)
   if (desiredForce_N <= 0) {
-    return 0.0f;
+  return 0.0f;
   }
 
   return sqrtf(desiredForce_N / propConstant);
 }
-
